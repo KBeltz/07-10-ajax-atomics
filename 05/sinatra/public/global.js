3ae8f16eefb64f00ae89d50c51bc8f130792f07e
Kristin Beltz <beltz08@me.com>
2015-07-13 09:05:46 -0500

    Move things around. Add [i] to getAttribute. Errors are gone but it isn't doing anything at this point so it's obviously not quite right yet.

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,8 +1,10 @@
 var finishLinks = document.getElementsByClassName("done_link");
-var link = finishLinks.getAttribute("href");
-var req = new XMLHttpRequest();
-req.open("get", link);
+
+
 
 for (var i=0; i < finishLinks.length; i++) {
   // finishLinks[i] represents each of the items in `finishLinks`.
+  var link = finishLinks[i].getAttribute("href");
+  var req = new XMLHttpRequest();
+  req.open("get", link);
 }
