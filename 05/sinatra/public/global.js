4e0a74cb3e85e135a16a007f7fbb671d1e63d0dc
Kristin Beltz <beltz08@me.com>
2015-07-13 10:07:50 -0500

    Some stuff. Event listeners and click handlers and such.

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,3 +1,7 @@
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 // returns an array of all elements with the class name 'done_link'
 var finishLinks = document.getElementsByClassName("done_link");
 // finishLinks[i] represents each of the items in `finishLinks`.
@@ -5,11 +9,31 @@ var link = finishLinks[i];
 
 for (var i=0; i < finishLinks.length; i++) {
   var handleDoneClick = function() {
-    var href = link.getAttribute("href");
+    // link.getAttribute("href") => gets the href value for each link
+    var href = this.getAttribute("href");
     // new XHR object
     var req = new XMLHttpRequest();
     req.open("get", href);
+
+    // load bar
+    req.addEventListener("loadstart", function() {
+      loadingGif = document.createElement("img");
+      loadingGif.setAttribute("src", "/ajax-loader.gif");
+      loadingGif.setAttribute("id", "loader");
+
+      insertAfter(loadingGif, event.target);
+    });
+
+    request.addEventListener("load", function() {
+      document.getElementById("loader").remove();
+      // removes the 'mark done' link
+      link.remove();
+    });
+
     req.send();
+
+    // prevents page from reloading when the link is clicked
+    event.preventDefault();
   }
   link.addEventListener("click", handleDoneClick);
 }
