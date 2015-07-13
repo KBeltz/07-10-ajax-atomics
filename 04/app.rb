8ef4a41c007d5bbbd7ae97a2d80619c0cc4c3703
Kristin Beltz <beltz08@me.com>
2015-07-12 21:26:52 -0500

    Close to finished with Exercise 3, getting internal server error 500.

--- 04/app.rb
+++ 04/app.rb
@@ -6,5 +6,13 @@ end
 
 get "/:msg" do
   message = params["msg"]
-  return "You wrote #{message}"
+  i = message.first
+
+  if ("A".."Z").include?(i)
+    return "You wrote #{message}. Is that someone's name?"
+  elsif ("0".."9").include?(i)
+    return "You wrote #{message}. Is that how old you are?"
+  else
+    return "You wrote #{message}. whatever."
+  end
 end
