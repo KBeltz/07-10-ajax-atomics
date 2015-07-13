3d0fe3d54eee23834ccbbb6ae99b4b0020d54b4d
Kristin Beltz <beltz08@me.com>
2015-07-13 09:33:33 -0500

    req.send() and event listener added.

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,10 +1,15 @@
+// returns an array of all elements with the class name 'done_link'
 var finishLinks = document.getElementsByClassName("done_link");
-
-
+// finishLinks[i] represents each of the items in `finishLinks`.
+var link = finishLinks[i];
 
 for (var i=0; i < finishLinks.length; i++) {
-  // finishLinks[i] represents each of the items in `finishLinks`.
-  var link = finishLinks[i].getAttribute("href");
-  var req = new XMLHttpRequest();
-  req.open("get", link);
+  var handleDoneClick = function() {
+    var href = link.getAttribute("href");
+    // new XHR object
+    var req = new XMLHttpRequest();
+    req.open("get", href);
+    req.send();
+  }
+  link.addEventListener("click", handleDoneClick);
 }
