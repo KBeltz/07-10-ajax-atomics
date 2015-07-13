034701e67001360e6754ddb014ad30c4b6bcab63
Kristin Beltz <beltz08@me.com>
2015-07-12 22:07:16 -0500

    Exercise 2 in prog. Will do more tomorrow.

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,4 +1,7 @@
 var finishLinks = document.getElementsByClassName("done_link");
+var link = finishLinks.getAttribute("href");
+var req = new XMLHttpRequest();
+req.open("get", link);
 
 for (var i=0; i < finishLinks.length; i++) {
   // finishLinks[i] represents each of the items in `finishLinks`.
