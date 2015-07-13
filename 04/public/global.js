ed61f29a9f8a83dac4f552fca22100555fb522af
Kristin Beltz <beltz08@me.com>
2015-07-12 21:04:35 -0500

    Exercise 1 complete.

--- 04/public/global.js
+++ 04/public/global.js
@@ -2,17 +2,17 @@ var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
   var thisLink = this;
-  
+
   var req = new XMLHttpRequest();
 
-  req.open("get", "/hey_there");
+  req.open("get", "/whats_up");
 
-  req.addEventListener("load", function(){    
+  req.addEventListener("load", function(){
     thisLink.innerText = this.response;
   });
 
   req.send();
-  
+
   // Prevent the link from refreshing the DOM.
   event.preventDefault();
 }
